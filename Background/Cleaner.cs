48030daa5aa954b064201cf98ad2637bff2b6730
lybanialove <nemmen222@bk.ru>
2023-09-08 16:43:28 +0300

    changes

--- Background/Cleaner.cs
+++ Background/Cleaner.cs
@@ -9,35 +9,48 @@ namespace RBTB_ServiceAnalitics.Background
 		private Timer _timer;
 		private TelegramClient _tg;
 
-		public Cleaner(AnaliticContext context)
+        public Cleaner(AnaliticContext context)
 		{
-			_context = context;
+
+            _context = context;
 			_context.Database.EnsureCreated();
 			_tg = new TelegramClient();
 		}
-		protected override Task ExecuteAsync( CancellationToken stoppingToken )
+		protected override async Task ExecuteAsync( CancellationToken stoppingToken )
 		{
-			TimerCallback call = new TimerCallback(Cleaning);
-			_timer = new Timer(call, null, 0, 3600000 ); // 1 час
-			return Task.CompletedTask;
-		}
-		public void Cleaning(object obj)
-		{
-			try
-			{
-				var dateFilter = DateTime.Now.AddDays( -1 );
-				var ticks = _context.Ticks
-					.Where( x => x.DateTime < dateFilter )
-					.ToList();
+            TimerCallback tm = new TimerCallback(Cleaning);
+            _timer = new Timer( tm, null, 0, 20);
+            if (stoppingToken.IsCancellationRequested) 
+            {
+                try
+                {
 
-				_context.Ticks.RemoveRange( ticks );
-				_context.SaveChanges();
-			}
-			catch ( Exception ex ) 
-			{
-				_tg.SendMessage("[ServiceAnalitic] - Упал в клининге");
-				_tg.SendMessage("[Cleaning] - " + ex.StackTrace);
-			}
+                }
+                catch 
+                {
+                    Console.WriteLine("Timed Hosted Service is stopping.");
+                }
+            }
+            await Task.CompletedTask;
 		}
-	}
+        public void Cleaning(object obj)
+        {
+            try
+            {
+                var dateFilter = DateTime.Now.AddDays(-1);
+                var ticks = _context.Ticks
+                    .Where(x => x.DateTime < dateFilter)
+                .ToList();
+
+                _context.Ticks.RemoveRange(ticks);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _tg.SendMessage("[ServiceAnalitic] - Упал в клининге");
+                _tg.SendMessage("[Cleaning] - " + ex.StackTrace);
+            }
+        }
+
+    }
 }
